a499e7033458710c9e4ce03184de302786d83d67
charlesdominique <charlesdominiquebacanto@gmail.com>
2025-01-27 10:40:21 +0800

    Comments

--- rightAlignedStairCase/Staircase.cs
+++ rightAlignedStairCase/Staircase.cs
@@ -8,22 +8,22 @@ namespace rightAlignedStairCase
 {
     internal class Staircase
     {
-        public static string step(int n, int m)
+        public static string step(int n, int m) // Method for creating the steps
         {
             string stair = "";
-            for (int i = 1; i <= m; i++)
+            for (int i = 1; i <= m; i++) // For loop for adding the space
             {
                 stair += ' ';
             }
-            for (int j = 1; j <= n; j++)
+            for (int j = 1; j <= n; j++) //For loop for adding the character step
             {
                 stair += '#';
             }
             return stair;
         }
-        public static void staircase(int n)
+        public static void staircase(int n) //Method for building the staircase
         {
-            for (int i = 1; i <= n; i++)
+            for (int i = 1; i <= n; i++) // For loop to set each step counts
             {
                 Console.WriteLine(step(i, n - i));
             }
--- rightAlignedStairCase/UserInput.cs
+++ rightAlignedStairCase/UserInput.cs
@@ -8,18 +8,19 @@ namespace rightAlignedStairCase
 {
     public static class UserInput
     {
-        private static int numValue;
+        private static int numValue; //Private declaration for staircase max value
 
-        public static int NumValue
+        public static int NumValue // Getters and Setters
         {
             get { return numValue; }
             set { numValue = value; }
         }
 
-        public static int getIntValue()
+        public static int getIntValue() // Get User Input of Max Value
         {
             Console.Write("Enter Max Staircase step: ");
             int NumValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
             return NumValue;
         }
     }
