64bef0050c771fbc316e60a413f791fe861ba0b9
charlesdominique <charlesdominiquebacanto@gmail.com>
2025-01-27 10:25:59 +0800

    Add project files.

---
+++ rightAlignedStairCase/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace rightAlignedStairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static string step(int n, int m)
+            {
+                string stair = "";
+                for (int i = 1; i <= m; i++)
+                {
+                    stair += ' ';
+                }
+                for (int j = 1; j <= n; j++)
+                {
+                    stair += '#';
+                }
+                return stair;
+            }
+            static void staircase(int n)
+            {
+                for(int i = 1; i <=n; i++)
+                {
+                    Console.WriteLine(step(i, n-i));
+                }
+            }
+
+            staircase(5);
+
+        }
+    }
+}
